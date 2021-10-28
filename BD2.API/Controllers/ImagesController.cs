630865a7f3dfa47a2a0edc80e3cc4e5362a966a8
Jakub Ciarka <01104656@pw.edu.pl>
2021-10-28 22:43:39 +0200

    Wyświetlanie zdjęc

---
+++ BD2.API/Controllers/ImagesController.cs
@@ -0,0 +1,36 @@
+﻿using BD2.API.Database.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BD2.API.Controllers
+{
+    [Route("[controller]")]
+    public class ImagesController : ExtendedControllerBase
+    {
+        public IImagesRepository _repo;
+        public ImagesController(IImagesRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> Image(Guid id)
+        {
+            var image = await _repo.FindAsync(id);
+
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return File(image.Binary, image.MimeType);
+        }
+
+    }
+}
